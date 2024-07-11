cd95ea757c207ccec7b7a211a2bbce378bd9b287
Zane <ericatkusa@gmail.com>
2024-07-11 16:56:14 +0530

    bool done

--- designBool.v
+++ designBool.v
@@ -1,6 +1,5 @@
-module bool(input a,b,c,output y);
+module boole(input a,b,c,output y);
 wire p1,p2,p3;
-reg y;
 assign y=p1+p2+p3;
 and(p1,a,b);
 and(p2,b,c);
--- testBool.v
+++ testBool.v
@@ -1,1 +1,19 @@
 module bool_tb();
+reg a,b,c;
+wire y;
+boole uut(.a(a),.b(b),.c(c),.y(y));
+initial begin
+$display("A\tB\tC\tY");
+$monitor("%b\t%b\t%b\t%b",a,b,c,y);
+$dumpfile("bool.vcd");
+$dumpvars(1);
+a=0;b=0;c=0;#10;
+a=0;b=0;c=1;#10;
+a=0;b=1;c=0;#10;
+a=0;b=1;c=1;#10;
+a=1;b=0;c=0;#10;
+a=1;b=0;c=1;#10;
+a=1;b=1;c=0;#10;
+a=1;b=1;c=1;#10;
+end;
+endmodule
